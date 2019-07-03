ab067980c7c3f1c8ce8e84bae932cae5371f4474
Elton Santana <faletaelton@gmail.com>
2019-07-03 16:29:43 -0300

    Set up the project for don't use Storyboard

--- TesteiOS-Zup/AppDelegate.swift
+++ TesteiOS-Zup/AppDelegate.swift
@@ -17,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Necessary Code to don't use storyboards
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let tabViewController = ViewController()
+        window!.rootViewController = tabViewController
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
